f00d4e7044a9d1b234ce36f25c1d9f564b2678c2
= <=>
2024-10-16 11:35:50 +0200

    Add refactored coin problem

--- week_03/pow/src/main.cpp
+++ week_03/pow/src/main.cpp
@@ -1,24 +1,29 @@
-///1
 #include <iostream>
 #include <vector>
 #include <cmath>
 
-int max_winnings(std::vector<int>* x, std::vector<std::vector<int>>* memo, int i, int j, int m, int k, int current) {
+/*
+- coin_values: contains the values for all n coins
+- memo: contains the precalculations for the subproblems
+- i: left index of the current interval
+- j: right index of the current interval
+- m: number of passengers
+- k: number of the person for which we are calculating the largest possible winnings
+- current: the number of the person which is currently choosing a coin
+*/
+int max_winnings(std::vector<int>* coin_values, std::vector<std::vector<int>>* memo, int i, int j, int m, int k, int current) {
   // only one element left
   if (i == j) {
     if (current == k) {
       // if its the turn of person k -> return coin value
-      return x->at(i);
+      return coin_values->at(i);
     } else {
       return 0; 
     }
   }
   
   // update index pointing to next person
-  int next = current + 1;
-  if (next == m) {
-    next = 0;
-  }
+  int next = (current + 1) % m;
   
   // gather memoisation data
   int right = memo->at(i+1).at(j);
@@ -26,42 +31,49 @@ int max_winnings(std::vector<int>* x, std::vector<std::vector<int>>* memo, int i
   
   // check if values need to be calculated
   if (right == -1) {
-    right = max_winnings(x, memo, i+1, j, m, k, next);
-    memo->at(i+1).at(j) = right;
+    right = max_winnings(coin_values, memo, i+1, j, m, k, next);
+    memo->at(i+1).at(j) = right;  // save calculated values
   }
   
   if (left == -1) {
-    left = max_winnings(x, memo, i, j-1, m, k, next);
-    memo->at(i).at(j-1) = left;
+    left = max_winnings(coin_values, memo, i, j-1, m, k, next);
+    memo->at(i).at(j-1) = left;   // save calculated values
   }
   
   if (current == k) {
     // its the turn of person k
+    
     // choose maximum winnings
-    return std::max((x->at(i) + right), (x->at(j) + left));
+    return std::max(
+      (coin_values->at(i) + right), // case 1: add left coin
+      (coin_values->at(j) + left)   // case 2: add right coin
+    );
   } else {
     // its not the turn of person k
+    
     // choose minimum winnings, as we cannot asume anything about their choices
-    return std::min(right, left); 
+    return std::min(
+      right,  // case 1
+      left    // case 2
+    ); 
   }
 }
 
 void testcase() {
   int n, m, k; std::cin >> n >> m >> k;
   
-  std::vector<int> x(n);
-  
   // initialize coin values
+  std::vector<int> coin_values(n);
+  
   for (int i = 0; i < n; i++) {
-    std::cin >> x.at(i);
+    std::cin >> coin_values.at(i);
   }
   
   // initialize memoization
   std::vector<std::vector<int>> memo(n, std::vector<int>(n, -1));
   
-  int max = max_winnings(&x, &memo, 0, n-1, m, k, 0);
-  
-  std::cout << max << std::endl;
+  // calculate max winnings for person k
+  std::cout << max_winnings(&coin_values, &memo, 0, n-1, m, k, 0) << std::endl;
 }
 
 int main() {
